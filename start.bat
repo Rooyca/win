c44dccb974d7043ae052fc4a3d1887132f134d48
Ronald Cantillo <42101257+Rooyca@users.noreply.github.com>
2023-08-18 14:41:43 +0000

    v1.0.0

--- start.bat
+++ start.bat
@@ -1,3 +1,7 @@
 @echo off
 
-echo Welcome
+start /b /wait curl https://raw.githubusercontent.com/Rooyca/win/main/logo.bmp --output %temp%\logo.bmp
+set "logopath=%temp%\logo.bmp"
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" /v "Logo" /t REG_SZ /d "%logopath%" /f
+
+irm https://christitus.com/win | iex; if ($?) { Write-Host "Success" } else { iwr -useb https://christitus.com/win | iex}
