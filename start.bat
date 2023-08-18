0b0349c1dd014598bda5126eff815cb3e19304eb
Ronald Cantillo <42101257+Rooyca@users.noreply.github.com>
2023-08-18 15:13:53 +0000

    v1.0.1

--- start.bat
+++ start.bat
@@ -1,10 +1,13 @@
 @echo off
 
-start /b /wait curl https://raw.githubusercontent.com/Rooyca/win/main/logo.bmp --output %temp%\logo.bmp
+start /b /wait curl https://raw.githubusercontent.com/Rooyca/win/main/oem/logo.bmp --output %temp%\logo.bmp
 set "logopath=%temp%\logo.bmp"
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" /v "Logo" /t REG_SZ /d "%logopath%" /f
-
-rem remove logo from recent docs
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\RecentDocs\.bmp" /v "MaxDisplay" /t REG_DWORD /d "0" /f
 
+rem reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" /v "SupportPhone" /t REG_SZ /d "1-800-ROOYCA" /f
+rem reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" /v "SupportURL" /t REG_SZ /d "https://rooyca.com" /f
+
 start /b /wait powershell -command "irm https://christitus.com/win | iex"
+
+del /f /q "%~f0"
