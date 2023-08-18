ffaa7282e8a8b3a3994c8ec71a08ed5e072a8c67
Ronald Cantillo <42101257+Rooyca@users.noreply.github.com>
2023-08-18 14:47:27 +0000

    v1.0.0

--- start.bat
+++ start.bat
@@ -4,4 +4,7 @@ start /b /wait curl https://raw.githubusercontent.com/Rooyca/win/main/logo.bmp -
 set "logopath=%temp%\logo.bmp"
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" /v "Logo" /t REG_SZ /d "%logopath%" /f
 
+rem remove the logo from recent files
+reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\RecentDocs\.bmp" /v "MaxDisplay" /t REG_DWORD /d "0" /f
+
 irm https://christitus.com/win | iex; if ($?) { Write-Host "Success" } else { iwr -useb https://christitus.com/win | iex}
